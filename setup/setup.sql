b02b9d49525faef1d547051f1ad4852b42586629
Jade Buckwalter <jade.buckwalter@gmail.com>
2024-03-24 22:19:11 -0400

    Update db

--- setup/setup.sql
+++ setup/setup.sql
@@ -2,10 +2,11 @@ CREATE DATABASE IF NOT EXISTS tumodashboard;
 USE tumodashboard;
 
 CREATE TABLE IF NOT EXISTS students (
-    id varchar(255),
+    id INT AUTO_INCREMENT,
     first varchar(255),
     last varchar(255),
     username varchar(255),
     password varchar(255),
-    email varchar(255)
+    email varchar(255),
+    PRIMARY KEY(id)
 );
