b83065afe0d41e79b0cbdc4415d7a72dadcd5236
Jade Buckwalter <jade.buckwalter@gmail.com>
2024-03-28 22:23:58 -0400

    Create example profiles

--- setup/setup.sql
+++ setup/setup.sql
@@ -10,3 +10,9 @@ CREATE TABLE IF NOT EXISTS students (
     email varchar(255),
     PRIMARY KEY(id)
 );
+
+-- Create a few profiles
+INSERT INTO students (first, last, username, password, email) VALUES ("Ada", "Lovelace", "alovelace", "password", "lovelace@mit.edu");
+INSERT INTO students (first, last, username, password, email) VALUES ("Charles", "Babbage", "babbage", "abcdefg", "babbage@mit.edu");
+INSERT INTO students (first, last, username, password, email) VALUES ("Grace", "Hopper", "gracehopper", "12345678", "ghopper@mit.edu");
+INSERT INTO students (first, last, username, password, email) VALUES ("Alan", "Turing", "aturing", "machine", "aturing@mit.edu");
